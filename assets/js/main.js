f6839b6698bc62d8d17c97ddefecc91f72023e93
Makarand Nikam <64835912+mack-1999@users.noreply.github.com>
2020-08-01 22:01:16 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-/*sr.reveal('.contact__button',{delay: 200}); */ 
+sr.reveal('.contact__button',{delay: 200});
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
 /*SCROLL ABOUT*/
